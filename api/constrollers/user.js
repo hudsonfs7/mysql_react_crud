0b084dccc4c27b032fa79727c0626796291fa825
Hudson Souza <hudsonfs7@hotmail.com>
2023-08-26 18:48:01 -0300

    Add methods POST, GET and PUT in backend

--- api/constrollers/user.js
+++ api/constrollers/user.js
@@ -1,5 +1,6 @@
 import { db } from '../db.js'
 
+// GET
 export const getUsers = (_, res) => {
   const q = 'SELECT * FROM users'
 
@@ -8,4 +9,42 @@ export const getUsers = (_, res) => {
 
     return res.status(200).json(data)
   })
+}
+
+// POST
+export const addUser = (req, res) => {
+  const q = 'INSERT INTO users(`name`, `email`, `phone`, `bdate`) VALUES(?)'
+
+  const VALUES = [req.body.name, req.body.email, req.body.phone, req.body.bdate]
+
+  db.query(q, [VALUES], err => {
+    if (err) return res.json(err)
+  })
+
+  return res.status(200).json('Usuário criado com sucesso')
+}
+
+// PUT
+export const updateUser = (req, res) => {
+  const q =
+    'UPDATE users SET `name` = ?, `email` = ?, `phone` = ?, `bdate` = ? WHERE `id` = ?'
+
+  const VALUES = [req.body.name, req.body.email, req.body.phone, req.body.bdate]
+
+  db.query(q, [...VALUES, req.params.id], err => {
+    if (err) return res.json(err)
+  })
+
+  return res.status(200).json('Usuário atualizado com sucesso')
+}
+
+// DELETE
+export const deleteUser = (req, res) => {
+  const q = 'DELETE FROM users WHERE `id` = ?'
+
+  db.query(q, [req.params.id], err => {
+    if (err) return res.json(err)
+  })
+
+  return res.status(200).json('Usuário deletado com sucesso')
 }
