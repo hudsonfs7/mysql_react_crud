50c77ad3e827b67b67305f96ff34fcbb5324d5b3
Hudson Souza <hudsonfs7@hotmail.com>
2023-08-26 14:35:06 -0300

    Form component created, with styled inputs. :smiley:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
+import Global from "./styles/global"
+import styled from 'styled-components'
+import Form from "./components/Form.jsx"
+import {toast, ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2`
+
+`
+
+
 
 function App() {
 
   return (
     <>
-      <h1>Hudson</h1>
+      <Container>
+      <Title>Usuários</Title>
+      <Form />
+
+      </Container>
+
+      
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <Global />
     </>
   )
 }
