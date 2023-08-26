b719bc439a4f84b5a12cd89105eb04c76b1c0786
Hudson Souza <hudsonfs7@hotmail.com>
2023-08-26 18:51:13 -0300

    Add routes to methods CRUD

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import { getUsers } from '../constrollers/user.js'
+import {
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser
+} from '../constrollers/user.js'
 
 const router = express.Router()
 
 router.get('/', getUsers)
+
+router.post('/', addUser)
+
+router.put('/:id', updateUser)
+
+router.put('/:id', deleteUser)
 
 export default router
